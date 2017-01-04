66793b58ef07732740970437cbbd42cf1cb92570
Tata <Tata@example.com>
2017-01-04 21:44:14 +0100

    testing work

--- src/BI/common/components/DisableWindowTask.java
+++ src/BI/common/components/DisableWindowTask.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 
 import javax.swing.JTabbedPane;
 
+//testing git
 public class DisableWindowTask implements Runnable {
 	JTabbedPane tabPane;
 	boolean enableOrDisable;
